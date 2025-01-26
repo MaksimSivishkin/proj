c633bf10a8982ea817d61e4e49c51d91467a9b42
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-01-26 12:38:19 +0300

    Доработка логики проекта 3

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -4,9 +4,6 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("10001");
-        System.out.println("22222");
-        System.out.println("33333");
-        System.out.println("33333");
         System.out.println("33333");
         System.out.println("33333");
         System.out.println("33333");
