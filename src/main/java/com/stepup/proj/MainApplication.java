92f5dedeace38f083f8d0b170ab50c8581da001c
Sivishkin Maksim <MSivishkin127@gmail.com>
2025-01-26 12:45:55 +0300

    Выполнение TASK0.0.1

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,14 +2,15 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         System.out.println("10001");
         System.out.println("33333");
         System.out.println("33333");
         System.out.println("33333");
         System.out.println("33333");
-        System.out.println("33333");
-        System.out.println("33333");
-        System.out.println("33333");
+    }
+
+    public static void DoSomething(String[] args) {
+        System.out.println("121212");
+
     }
 }
